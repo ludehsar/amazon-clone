3d971108dfde981fcd4e56043dc4a309d53364f1
Rashedul Alam <rashedulalam112112@gmail.com>
2020-09-21 13:32:31 +0600

    Improved product component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,15 +10,53 @@ function Home() {
         <img className="home__banner" src="/banner.jpg" alt="Prime banner" />
 
         <div className="home__row">
-          <Product />
-          {/* Product */}
+          <Product
+            id="1"
+            title="Logitech G602 Lag-Free Wireless Gaming Mouse – 11 Programmable Buttons, Upto 2500 DPI"
+            price={11.86}
+            rating={5}
+            image="/product1.jpg"
+          />
+          <Product
+            id="2"
+            title="Samsung (MZ-V7S1T0B/AM) 970 EVO Plus SSD 1TB - M.2 NVMe Interface Internal Solid State Drive with V-NAND Technology"
+            price={179.99}
+            rating={3}
+            image="/product2.jpg"
+          />
         </div>
         <div className="home__row">
-          {/* Product */}
-          {/* Product */}
-          {/* Product */}
+          <Product
+            id="3"
+            title="BENGOO G9000 Stereo Gaming Headset for PS4, PC, Xbox One Controller, Noise Cancelling Over Ear Headphones with Mic, LED Light, Bass Surround, Soft Memory Earmuffs for Laptop Mac Nintendo PS3 Games"
+            price={36.99}
+            rating={4}
+            image="/product3.jpg"
+          />
+          <Product
+            id="1"
+            title="Logitech G602 Lag-Free Wireless Gaming Mouse – 11 Programmable Buttons, Upto 2500 DPI"
+            price={11.86}
+            rating={5}
+            image="/product1.jpg"
+          />
+          <Product
+            id="1"
+            title="Logitech G602 Lag-Free Wireless Gaming Mouse – 11 Programmable Buttons, Upto 2500 DPI"
+            price={11.86}
+            rating={5}
+            image="/product1.jpg"
+          />
         </div>
-        <div className="home__row">{/* Product */}</div>
+        <div className="home__row">
+          <Product
+            id="1"
+            title="Logitech G602 Lag-Free Wireless Gaming Mouse – 11 Programmable Buttons, Upto 2500 DPI"
+            price={11.86}
+            rating={5}
+            image="/product1.jpg"
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,27 +3,29 @@ import StarIcon from "@material-ui/icons/Star";
 
 import "./Product.css";
 
-function Product() {
+function Product(props) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>
-          Logitech G602 Lag-Free Wireless Gaming Mouse – 11 Programmable
-          Buttons, Upto 2500 DPI
-        </p>
+        <p class="product__title">{props.title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>86.00</strong>
+          <strong>{props.price}</strong>
         </p>
         <div className="product__rating">
-          <StarIcon className="product__ratingGold" />
-          <StarIcon className="product__ratingGold" />
-          <StarIcon className="product__ratingGold" />
-          <StarIcon className="product__ratingWhite" />
-          <StarIcon className="product__ratingWhite" />
+          {Array(props.rating)
+            .fill()
+            .map((_) => (
+              <StarIcon className="product__ratingGold" />
+            ))}
+          {Array(5 - props.rating)
+            .fill()
+            .map((_) => (
+              <StarIcon className="product__ratingWhite" />
+            ))}
         </div>
       </div>
-      <img src="/product.jpg" alt="Mouse" />
+      <img src={props.image} alt="Mouse" />
       <button>Add to Basket</button>
     </div>
   );
